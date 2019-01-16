529db8506f062835e51eba59dee54485b95f1722
developer <developer@instance.vnc>
2019-01-16 17:04:29 +0000

    separe classes

---
+++ lib/MultipleWitgets.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+class MultipleWitgets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                  Expanded(
+                    flex: 2,
+                    child: Image.asset('images/pic2.jpg'),
+                  ),
+                  Expanded(
+                    child: Image.asset('images/pic3.jpg'),
+                  ),
+                  Expanded(
+                    child: Image.asset('images/pic1.jpg'),
+                  ),
+                ])),
+            Expanded(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.star, color: Colors.green[500]),
+                  Icon(Icons.star, color: Colors.green[500]),
+                  Icon(Icons.star, color: Colors.green[500]),
+                  Icon(Icons.star, color: Colors.black),
+                  Icon(Icons.star, color: Colors.black),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MyHomePageState2 extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var packedRow = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.black),
+        Icon(Icons.star, color: Colors.black),
+      ],
+    );
+  }
+}
